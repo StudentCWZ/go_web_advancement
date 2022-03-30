85b825306352e4ef50fadddf8e42fac69588c735
cuiweizhi <330459539@qq.com>
2022-03-30 17:23:38 +0800

    lesson 29

--- lesson29/bluebell/router/router.go
+++ lesson29/bluebell/router/router.go
@@ -46,6 +46,9 @@ func Setup() *gin.Engine {
 	v1.POST("/signup", controller.SignUpHandler)
 	v1.POST("/login", controller.LoginHandler)
 	{
+		v1.GET("/community", controller.CommunityHandler)
+		v1.GET("/community/:id", controller.CommunityDetailHandler)
+		v1.GET("/post/:id", controller.GetPostDetailHandler)
 		v1.GET("/posts", controller.GetPostListHandler)
 		// 根据时间或分数获取帖子列表
 		v1.GET("/posts2", controller.GetPostListTwoHandler)
@@ -53,9 +56,6 @@ func Setup() *gin.Engine {
 
 	v1.Use(middlewares.JWTAuthMiddleware(), middlewares.RateLimitMiddleware(2*time.Second, 1)) // 应用 JWT 认证中间件以及令牌桶限流中间件
 	{
-		v1.GET("/community", controller.CommunityHandler)
-		v1.GET("/community/:id", controller.CommunityDetailHandler)
-		v1.GET("/post/:id", controller.GetPostDetailHandler)
 		v1.POST("/post", controller.CreatePostHandler)
 		v1.POST("/vote", controller.PostVoteHandler)
 	}
